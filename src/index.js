4fdc69e0ed2c765de51fa8c3b74e88af95aae317
Micael <micaelxv1@gmail.com>
2025-01-08 22:49:10 -0300

    primeiros passos

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); // renderiza o app dentro de document.getElementById('root'), ele joga esse componente na div da index com o id root
 root.render(
   <React.StrictMode>
     <App />
