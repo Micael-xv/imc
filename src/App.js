3450db2bbbbc53a412610000094f772259b8b166
Micael <micaelxv1@gmail.com>
2025-01-10 21:47:46 -0300

    primeiros passos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 function App() {
-  return ( // dentro do return eu coloco o que eu quero que seja renderisado
+  return ( // dentro do return eu coloco o que eu quero que seja renderisado!
     <div>
       <h1>Calculadora de imc</h1>
     </div>
