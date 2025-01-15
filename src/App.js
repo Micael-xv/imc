65ff4d011b54fc41d7b646894e1c71892468cf1d
Micael <micaelxv1@gmail.com>
2025-01-14 21:49:13 -0300

    imc

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-function App() {
+export default function App() { // isso é um componente e precisar ser esportado, então é usado o export default, e é importado no index.js
   return ( // dentro do return eu coloco o que eu quero que seja renderisado!
     <div>
       <h1>Calculadora de imc</h1>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App'; // aqui eu estou importando o component 
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // renderiza o app dentro de document.getElementById('root'), ele joga esse componente na div da index com o id root
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> {/* aqui eu estou renderizando o componente App */}
   </React.StrictMode>
 );
