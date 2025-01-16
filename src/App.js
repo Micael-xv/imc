283a01b8054aa802be4dc05218f0d8a63a2e2a27
Micael <micaelxv1@gmail.com>
2025-01-15 22:16:39 -0300

    forget

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 export default function App() { // isso é um componente e precisar ser esportado, então é usado o export default, e é importado no index.js
   return ( // dentro do return eu coloco o que eu quero que seja renderisado!
-    <div>
+    <div className="app">
       <h1>Calculadora de imc</h1>
+      <span>Vamos calcular seu imc</span>
+
+      <div className="area-input">
+        <input 
+          type="text" 
+          placeholder="Peso em (kg) Ex: 75" 
+        />
+        <input 
+          type="text" 
+          placeholder="Altura em (cm) Ex: 185" 
+        />
+        <button>Calcular</button>
+      </div>
     </div>
   )
 }
